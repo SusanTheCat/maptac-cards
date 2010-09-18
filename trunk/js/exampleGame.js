36f44aba45a5bad35d67738be3b1a3241549ee0e
tikhonjelvis <tikhonjelvis@5df8c2e1-db65-ddc7-73c1-4aeb235e3af1>
2010-09-18 09:37:33 +0000

    Updated example game to use the new pile constructor.

--- trunk/js/exampleGame.js
+++ trunk/js/exampleGame.js
@@ -2,9 +2,9 @@ function ExampleGame() {
     var game = new CardGame();
     var pile1 = new Pile(game);
     var pile2Pos = new Position(pile1, {left : 1});
-    var pile2 = new Pile(game, {position : pile2Pos});
+    var pile2 = new Pile(game, pile2Pos);
     var deck = game.getDeck();
-	deck.shuffle();
+    deck.shuffle();
     for (var i = 0; i < deck.length; i++) {
 		if (Math.random() < 0.5) {
 			pile1.addCard(deck[i]);

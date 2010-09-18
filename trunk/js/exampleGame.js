43aa6d827955f9ac05e37e6321f975fa4165e993
tikhonjelvis <tikhonjelvis@5df8c2e1-db65-ddc7-73c1-4aeb235e3af1>
2010-09-18 04:30:57 +0000

    Added an example game and finished various model parts needed for implementing solitaire, ideally.

---
+++ trunk/js/exampleGame.js
@@ -0,0 +1,17 @@
+function ExampleGame() {
+    var game = new CardGame();
+    var pile1 = new Pile(game);
+    var pile2Pos = new Position(pile1, {left : 1});
+    var pile2 = new Pile(game, {position : pile2Pos});
+    var deck = game.getDeck();
+    for (var i = 0; i < deck.length; i++) {
+	if (Math.random() < 0.5) {
+	    pile1.addCard(deck[i]);
+	} else {
+	    pile2.addCard(deck[i]);
+	}
+    }
+
+    game.addPile(pile1);
+    game.addPile(pile2);
+}

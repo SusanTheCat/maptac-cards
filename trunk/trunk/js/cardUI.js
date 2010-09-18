769f41924e652d32f691de9cc69cc51f536a6aa6
swang2014@berkeley.edu <swang2014@berkeley.edu@5df8c2e1-db65-ddc7-73c1-4aeb235e3af1>
2010-09-18 03:19:34 +0000

    Fixed typos in cardUI.js.

--- trunk/trunk/js/cardUI.js
+++ trunk/trunk/js/cardUI.js
@@ -7,56 +7,56 @@ function createCard(card) {
 		+"0000'>"+rank+"</div><img src='img/"+suit+".png' height='18' width='18'>"; /* Ternary operator to determine card color*/
 	switch (rank) {
 		case 3: 
-			d.innerHTML += "<div class='b3'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='b3'><img src='img/"+suit+".png'></div>";
 		case 2: 
-			d.innerHTML += "<div class='b1'><img src='img/"+suit+".png'30></div>"
-						+  "<div class='b5'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='b1'><img src='img/"+suit+".png'></div>"
+						+  "<div class='b5'><img src='img/"+suit+".png'></div>";
 			break;
 		case "A": 
 			d.innerHTML += "<div class='a1'><br /><br /><img src='img/"+suit+".png' height='70' width='70'></div>";
 			break;
 		case 8:
-			d.innerHTML += "<div class='b4'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='b4'><img src='img/"+suit+".png'></div>";
 		case 7:
-			d.innerHTML += "<div class='b2'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='b2'><img src='img/"+suit+".png'></div>";
 		case 6:
-			d.innerHTML += "<div class='a3'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c3'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='a3'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c3'><img src='img/"+suit+".png'></div>";
 		case 4:
-			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'></div>";
 			break;
 		case 5:
-			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='b3'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='b3'><img src='img/"+suit+".png'></div>";
 			break;
 		case 9:
-			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a2'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a4'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c2'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c4'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='b3'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a2'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a4'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c2'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c4'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='b3'><img src='img/"+suit+".png'></div>";
 			break;
 		case 10:
-			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a2'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a4'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c2'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c4'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='b2'><img src='img/"+suit+".png'30></div>";
-			d.innerHTML += "<div class='b4'><img src='img/"+suit+".png'30></div>";
+			d.innerHTML += "<div class='a1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a2'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a4'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='a5'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c1'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c2'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c4'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='c5'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='b2'><img src='img/"+suit+".png'></div>";
+			d.innerHTML += "<div class='b4'><img src='img/"+suit+".png'></div>";
 	}
 	return d;
 }

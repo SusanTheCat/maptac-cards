af6847bf0cbdbe806c4b53ced3a20d87ced38f91
swang2014@berkeley.edu <swang2014@berkeley.edu@5df8c2e1-db65-ddc7-73c1-4aeb235e3af1>
2010-09-18 02:10:56 +0000

    FIxed bugs with cardUI.js, made slight cosmetic changes.

--- trunk/trunk/js/cardUI.js
+++ trunk/trunk/js/cardUI.js
@@ -1,8 +1,9 @@
 function createCard(card) {
     var suit = card.getSuit();
     var rank = card.getRank();
-    var div = document.createElement("div");
-	div.class = "card";
-	div.innerHTML = "<span style='color:#"+(rank=="h"||rank=="d")?"FF0000":"#000000"
-		+">"+rank+"</span><br /><img src='img/"+suit+".png' height='15' width='15'>"; /* Ternary operator to determine card color*/
+    var d = document.createElement("div");
+	d.setAttribute("class", "card");
+	d.innerHTML = "<div style='width:20px;text-align:center;color:#"+((suit=="h"||suit=="d")?"FF":"00")
+		+"0000'>"+rank+"</div><img src='img/"+suit+".png' height='18' width='18'>"; /* Ternary operator to determine card color*/
+	document.getElementById("game").insertBefore(d);
 }

9bcef779eafa34ef2f28706b67e0616644c7f1cc
tikhonjelvis <tikhonjelvis@5df8c2e1-db65-ddc7-73c1-4aeb235e3af1>
2010-09-18 08:07:41 +0000

    Forgot to add this file last time...

---
+++ trunk/js/solitaire.js
@@ -0,0 +1,34 @@
+/* This is a simple game of solitaire with out library! */
+
+function Solitaire() {
+    var game = new CardGame();
+
+    var mainPile = new Pile(game, new Position(), {faceDown : true});
+
+    var discardPile = new Pile(game, new Position(mainPile));
+
+    var spacer = new Spacer(game, 0.5, new Position(discardPile));
+
+    var suitPiles = [];
+    suitPiles.push(new Pile(game, new Position(spacer)));
+    for (var i = 0; i < 3; i++) {
+	suitPiles.push(new Pile(game, new Position(suitPiles[i])));
+    }
+
+    var downPiles = [];
+
+    var downPos = new Position(mainPile, {top : 1, left : 1});
+    downPiles.push(new Pile(game, downPos));
+    game.dealToPile(leftMostPile, 1);
+
+    for (var i = 0; i < 6; i++) {
+	var downPile = new Pile(game, new Position(downPiles[i]), 
+				{faceDown : true});
+	game.dealToPile(downPile, i + 1);
+	var upPile = new Pile(game, new Position(downPile, 
+						 {z : 1, left : 0}));
+	game.dealToPile(upPile, 1);
+	downPiles.push(downPile);
+	downPiles.push(upPile);
+    }
+}

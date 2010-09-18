b079f053cfa175fffe77eea045c970dee383e05e
tikhonjelvis <tikhonjelvis@5df8c2e1-db65-ddc7-73c1-4aeb235e3af1>
2010-09-18 02:02:08 +0000

    Fixed a bug in Card#getRank; added a working shuffle method to deck; some other stuff.

--- trunk/trunk/js/cardGame.js
+++ trunk/trunk/js/cardGame.js
@@ -2,10 +2,25 @@
 
 /* Represents the actual game; this is what gets and updates observers! */
 function CardGame(options) {
+    options = options || {};
     var deck = options.deck || Deck();
 
+    var observers = [];// All observers to be notified of changes.
+
+    /* Returns the deck that this game is using. */
     this.getDeck = function () {
 	return deck;
+    };
+
+    /* Fires the given event to all of the observers. */
+    function fire(event) {
+	for (var i = 0; i < observers.length; i++) {
+	    try {
+		observers[i](event);
+	    } catch (x) {
+		delete observers[i];
+	    }
+	}
     }
 }
 
@@ -18,7 +33,7 @@ function Deck(cards) {
     /* Creates the default 52 card deck. */
     function createCards() {
 	var newCards = [];
-	for (var i = 0; i < 13; i++) {
+	for (var i = 1; i <= 13; i++) {
 	    newCards.push(new Card(i, "s"));
 	    newCards.push(new Card(i, "h"));
 	    newCards.push(new Card(i, "d"));
@@ -27,6 +42,27 @@ function Deck(cards) {
 
 	return newCards;
     }
+
+    /* Shuffles the deck using some shuffling algorithm. */
+    cards.shuffle = function () {
+	for (var i = 0; i < cards.length; i++) {
+	    var card = cards[i];
+	    var rnum = Math.floor(Math.random() * (cards.length - i) + i);
+	    cards[i] = cards[rnum];
+	    cards[rnum] = card;
+	}
+    };
+
+    /* Returns an array of the string representations of the cards. */
+    cards.toStringArray = function () {
+	var stringArray = [];
+
+	for (var i = 0; i < cards.length; i++) {
+	    stringArray.push(cards[i].toString());
+	}
+	
+	return stringArray;
+    };
 
     return cards;
 }
@@ -55,12 +91,20 @@ function Card(rank, suit) {
      */
     this.getSuit = function () {
 	return suit;
-    }
-
+    };
     /* Returns the cards rank which is a number or a letter corresponding to
      * various face cards (ace: a, jack: j, queen: q and king: k).
      */
     this.getRank = function () {
 	return rank;
-    }
+    };
+    
+    /* Returns a simple string representation of the card with one to two digits
+     * for the rank and a single letter for the suit. Face cards get special
+     * letters instead of digits, naturally. This includes aces. Joker support will
+     * be added later.
+     */
+    this.toString = function () {
+	return rank + suit;
+    };
 }
